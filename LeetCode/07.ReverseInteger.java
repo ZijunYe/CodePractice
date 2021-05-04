be350c8d90f1b84b7a8670cf48893fcb12924f95
Joy Ye <zijunye001@gmail.com>
2021-05-04 17:25:10 -0400

    09done

--- LeetCode/07.ReverseInteger.java
+++ LeetCode/07.ReverseInteger.java
@@ -25,7 +25,6 @@ class ReverseInteger {
   */
    	public int reverse(int x) {
         long res = 0;
-        // 考虑负数情况，所以这里条件为: x != 0
         while (x != 0) {
             res = res * 10 + (x % 10);
             x /= 10;
