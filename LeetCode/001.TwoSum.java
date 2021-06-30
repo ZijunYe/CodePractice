d64fd0f01789e9991ac5020ff385b4ec9112db4f
Joy Ye <zijunye001@gmail.com>
2021-06-30 10:32:17 -0400

    two sum

--- LeetCode/001.TwoSum.java
+++ LeetCode/001.TwoSum.java
@@ -1,7 +1,7 @@
 // 000 two sum 
 
 class TwoSum {
-    public int[] twoSum(int[] nums, int target) { 
+    public int[] twoSum(int[] nums, int target) { //DOUBLE LOOP 
         int [] output = new int[2];
         int check = 0; 
         
@@ -16,6 +16,32 @@ class TwoSum {
             }
         }
         return null;
+    }
+
+
+    public int[] twoSum2(int[] nums,int target){ // hashMap 
+
+        //first we need to create an array that store result 
+        int[] result = new int[2]; 
+        if(nums.length == 0 || nums == null){
+            return result; 
+        }
+
+
+        //create map 
+        Map<Integer,Integer> hashMap = new HashMap<Integer,Integer>(); 
+        for(int i = 0; i < nums.length; i++){
+            int tmp = target - nums[i];
+            if(hashMap.containsKey(tmp)){
+                result[1] = i;
+                result[0]=hashMap.get(tmp); 
+            }
+
+            hashMap.put(nums[i],i); 
+        }
+
+        return result; 
+
     }
  }
 
